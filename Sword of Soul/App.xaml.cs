c93d0147000f6e15cfd0cf0eb714f8837a9cc649
Jazer <saltanhayrliev2006@gmail.com>
2024-10-16 02:31:29 +0300

    first commit -add startAnimation

---
+++ Sword of Soul/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Sword_of_Soul
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
